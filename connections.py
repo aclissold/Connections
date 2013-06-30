1344afd2c95308137a0b15abdc7389778656b801
Andrew Clissold <andrewclissold@gmail.com>
2013-06-30 14:21:13 -0400

    Fixed bug with coloring winning pieces

--- connections.py
+++ connections.py
@@ -3,16 +3,22 @@
 # A simple command-line Connect Four clone.
 #
 # To-do:
-#   Allow winning of five+ in a row
 #   Colorize winning pieces on Windows
-#   Add comments/docstrings
+#   Refactor:
+#       Add comments/docstrings
+#       Fix lines >= 79 characters
+#       Consider using loops to draw the board
+#       Make classe(s)
+#       Combine functionality of win() and draw_board()
+#       board drawing methods
+#   Create online version
 import os, sys
 from random import randint
 
 # Initialize board to empty cells
 board = [[[] for index in range(7)] for index in range(7)]
-winning_rows = [7, 7, 7, 7, 7, 7, 7]
-winning_columns = [7, 7, 7, 7, 7, 7, 7]
+winning_rows = []
+winning_columns = []
 turn = randint(1, 2)
 last_row = 0
 last_column = 0
@@ -22,6 +28,9 @@ def reset_board():
     for i in range(7):
         for j in range(7):
             board[i][j] = ' '
+    for i in range(len(winning_rows)):
+        winning_rows.pop()
+        winning_columns.pop()
 
 # Display the current board state to the user,
 # in a pretty format.
@@ -101,117 +110,133 @@ def top_row_full():
 def has_four_in_a_row():
     dropped_piece = board[last_row][last_column]
     has_four_in_a_row = False
-    if dropped_piece == 'x' or dropped_piece == 'o':
-        # Check if there's four-in-a-row horizontally
-        count = 1 # Solely used for winning_rows and columns
-        winning_rows[0] = last_row
-        winning_columns[0] = last_column
-        in_a_row = 1
-        for i in range(1, 5):
-            if last_column - i >= 0:
-                if board[last_row][last_column - i] == dropped_piece:
-                    in_a_row += 1
-                    winning_rows[count] = last_row
-                    winning_columns[count] = last_column - i
-                    count += 1
-                else:
-                    break
+    winning_rows.append(last_row)
+    winning_columns.append(last_column)
+    in_a_row = 1
+    # Check if there's four-in-a-row horizontally
+    for i in range(1, 7):
+        # Right to left
+        if last_column - i >= 0:
+            if board[last_row][last_column - i] == dropped_piece:
+                in_a_row += 1
+                winning_rows.append(last_row)
+                winning_columns.append(last_column - i)
             else:
                 break
-        for i in range(1, 5):
-            if last_column + i <= 6:
-                if board[last_row][last_column + i] == dropped_piece:
-                    in_a_row += 1
-                    winning_rows[count] = last_row
-                    winning_columns[count] = last_column + i
-                    count += 1
-                else:
-                    break
+        else:
+            break
+    for i in range(1, 7):
+        # Left to right
+        if last_column + i <= 6:
+            if board[last_row][last_column + i] == dropped_piece:
+                in_a_row += 1
+                winning_rows.append(last_row)
+                winning_columns.append(last_column + i)
             else:
                 break
-        if in_a_row >= 4:
-            has_four_in_a_row = True
-        # Check if there's four-in-a-row vertically
-        count = 1
-        in_a_row = 1
-        for i in range(1, 5):
-            if last_row + i <= 6:
-                if board[last_row + i][last_column] == dropped_piece:
-                    in_a_row += 1
-                    winning_rows[count] = last_row + i
-                    winning_columns[count] = last_column
-                    count += 1
-                else:
-                    break
+        else:
+            break
+    if in_a_row >= 4:
+        has_four_in_a_row = True
+    else:
+        if in_a_row > 1:
+            for i in range(in_a_row - 1):
+                winning_rows.pop()
+                winning_columns.pop()
+            in_a_row = 1
+    # Check if there's four-in-a-row vertically
+    for i in range(1, 5):
+        # Bottom to top
+        if last_row + i <= 6:
+            if board[last_row + i][last_column] == dropped_piece:
+                in_a_row += 1
+                winning_rows.append(last_row + i)
+                winning_columns.append(last_column)
             else:
                 break
-        for i in range(1, 5):
-            if last_row - i >= 0:
-                if board[last_row - i][last_column] == dropped_piece:
-                    in_a_row += 1
-                    winning_rows[count] = last_row - i
-                    winning_columns[count] = last_column
-                    count += 1
-                else:
-                    break
+        else:
+            break
+    for i in range(1, 5):
+        # Top to bottom
+        if last_row - i >= 0:
+            if board[last_row - i][last_column] == dropped_piece:
+                in_a_row += 1
+                winning_rows.append(last_row - i)
+                winning_columns.append(last_column)
             else:
                 break
-        if in_a_row >= 4:
-            has_four_in_a_row = True
-        # Check if there's four-in-a-row diagonally (/)
-        count = 1
-        in_a_row = 1
-        for i in range(1, 5):
-            if last_row + i <= 6 and last_column + i <= 6:
-                if board[last_row + i][last_column + i] == dropped_piece:
-                    in_a_row += 1
-                    winning_rows[count] = last_row + i
-                    winning_columns[count] = last_column + i
-                    count += 1
-                else:
-                    break
+        else:
+            break
+    if in_a_row >= 4:
+        has_four_in_a_row = True
+    else:
+        if in_a_row > 1:
+            for i in range(in_a_row - 1):
+                winning_rows.pop()
+                winning_columns.pop()
+            in_a_row = 1
+    # Check if there's four-in-a-row diagonally (/)
+    for i in range(1, 7):
+        # Lower-left to upper-right
+        if last_row + i <= 6 and last_column + i <= 6:
+            if board[last_row + i][last_column + i] == dropped_piece:
+                in_a_row += 1
+                winning_rows.append(last_row + i)
+                winning_columns.append(last_column + i)
             else:
                 break
-        for i in range(1, 5):
-            if last_row - i >= 0 and last_column - i >= 0:
-                if board[last_row - i][last_column - i] == dropped_piece:
-                    in_a_row += 1
-                    winning_rows[count] = last_row - i
-                    winning_columns[count] = last_column - i
-                    count += 1
-                else:
-                    break
+        else:
+            break
+    for i in range(1, 7):
+        # Upper-right to lower-left
+        if last_row - i >= 0 and last_column - i >= 0:
+            if board[last_row - i][last_column - i] == dropped_piece:
+                in_a_row += 1
+                winning_rows.append(last_row - i)
+                winning_columns.append(last_column - i)
             else:
                 break
-        if in_a_row >= 4:
-            has_four_in_a_row = True
-        # Check if there's four-in-a-row diagonally (\)
-        count = 1
-        in_a_row = 1
-        for i in range(1, 5):
-            if last_row - i >= 0 and last_column + i <= 6:
-                if board[last_row - i][last_column + i] == dropped_piece:
-                    in_a_row += 1
-                    winning_rows[count] = last_row - i
-                    winning_columns[count] = last_column + i
-                    count += 1
-                else:
-                    break
+        else:
+            break
+    if in_a_row >= 4:
+        has_four_in_a_row = True
+    else:
+        if in_a_row > 1:
+            for i in range(in_a_row - 1):
+                winning_rows.pop()
+                winning_columns.pop()
+            in_a_row = 1
+    # Check if there's four-in-a-row diagonally (\)
+    for i in range(1, 7):
+        if last_row - i >= 0 and last_column + i <= 6:
+            if board[last_row - i][last_column + i] == dropped_piece:
+                in_a_row += 1
+                winning_rows.append(last_row - i)
+                winning_columns.append(last_column + i)
             else:
                 break
-        for i in range(1, 5):
-            if last_row + i <= 6 and last_column - i >= 0:
-                if board[last_row + i][last_column - i] == dropped_piece:
-                    in_a_row += 1
-                    winning_rows[count] = last_row + i
-                    winning_columns[count] = last_column - i
-                    count += 1
-                else:
-                    break
+        else:
+            break
+    for i in range(1, 7):
+        if last_row + i <= 6 and last_column - i >= 0:
+            if board[last_row + i][last_column - i] == dropped_piece:
+                in_a_row += 1
+                winning_rows.append(last_row + i)
+                winning_columns.append(last_column - i)
             else:
                 break
-        if in_a_row >= 4:
-            has_four_in_a_row = True
+        else:
+            break
+    if len(winning_rows) >= 4:
+        has_four_in_a_row = True
+    else:
+        if in_a_row > 1:
+            for i in range(in_a_row - 1):
+                winning_rows.pop()
+                winning_columns.pop()
+        # Also remove initial piece
+        winning_rows.pop()
+        winning_columns.pop()
     return has_four_in_a_row
 
 def win():
@@ -219,11 +244,8 @@ def win():
     # Change colors of the winning pieces
     piece = 'x' if turn == 2 else 'o'
     piece = '\033[38;5;118m' + piece + '\033[0m'
-    for i in range(7):
-        if winning_rows[i] < 7 and winning_columns[i] < 7:
-            board[winning_rows[i]][winning_columns[i]] = piece
-        else: # End unnecessary checks
-            break
+    for i in range(len(winning_rows)):
+        board[winning_rows[i]][winning_columns[i]] = piece
     # Basically draw_board()
     print()
     print(' ~DING DING DING!~'.format(turn))
