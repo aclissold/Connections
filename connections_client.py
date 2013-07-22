60dbb48041936e18fc9a2bbf67c65ffb300275a6
Andrew Clissold <ajclisso@oakland.edu>
2013-07-22 14:48:03 -0400

    Allow two clients to connect to the server

--- connections_client.py
+++ connections_client.py
@@ -1,21 +1,70 @@
 #!/usr/bin/env python3
+import os
 import socket
+import threading
 
 HOST = 'localhost'
 PORT = 50007
 
-# X's
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-s.sendall(b'xHello, world')
-data = s.recv(1024)
-s.close()
-print(data.decode('utf-8'))
+player = int(input("Player 1 or 2: "))
+#os.system('clear')
 
-# O's
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-s.sendall(b'oHello, world')
-data = s.recv(1024)
-s.close()
-print(data.decode('utf-8'))
+class SendThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        if player == 1:
+            # X's
+            while True:
+                msg = "1" + input()
+                msg = bytes(msg, 'ascii')
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((HOST, PORT))
+                s.sendall(msg)
+                s.close()
+        elif player == 2:
+            # O's
+            while True:
+                msg = "2" + input()
+                msg = bytes(msg, 'ascii')
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((HOST, PORT))
+                s.sendall(msg)
+                s.close()
+        else:
+            raise Exception("You must enter a 1 or 2.")
+
+class RecvThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        if player == 1:
+            # X's
+            while True:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((HOST, PORT))
+                s.sendall(b'3')
+                data = (s.recv(1024))
+                if data != b'':
+                    data = str(data, 'utf-8')
+                    print(data)
+                s.close()
+        elif player == 2:
+            # O's
+            while True:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((HOST, PORT))
+                s.sendall(b'0')
+                data = s.recv(1024)
+                if data != b'':
+                    data = str(data, 'utf-8')
+                    print(data)
+                s.close()
+        else:
+            raise Exception("You must enter a 1 or 2.")
+
+sendThread = SendThread()
+recvThread = RecvThread()
+
+sendThread.start()
+recvThread.start()
--- connections_server.py
+++ connections_server.py
@@ -2,26 +2,57 @@
 # Test server
 
 import socket
+import threading
+import socketserver
 
-HOST = ''
-PORT = 50007
-while True:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind((HOST, PORT))
-    sock.listen(1)
-    conn, addr = sock.accept()
-    while True:
-        data = conn.recv(1024)
-        if not data: break
-        data = data.decode('utf-8')
-        if data[0] == 'x':
-            print("Received connection from Player 1")
-        elif data[0] == 'o':
-            print("Received connection from Player 2")
+newMessage1 = newMessage2 = False
+data1 = data2 = bytes("test", "ascii")
+
+class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        data = str(self.request.recv(1024), 'ascii')
+        global data1, data2
+        if data[0] == '0':
+            global newMessage1
+            if newMessage1:
+                self.request.sendall(data1)
+                newMessage1 = False
+            else:
+                while not newMessage1:
+                    if newMessage1:
+                        self.request.sendall(data1)
+                        newMessage1 = False
+        elif data[0] == '3':
+            global newMessage2
+            if newMessage2:
+                self.request.sendall(data2)
+                newMessage2 = False
+            else:
+                while not newMessage1:
+                    if newMessage2:
+                        self.request.sendall(data2)
+                        newMessage2 = False
+        elif data[0] == '1':
+            data1 = bytes("Rachel: " + data[1:], 'ascii')
+            newMessage1 = True
+        elif data[0] == '2':
+            data2 = bytes("Andrew: " + data[1:], 'ascii')
+            newMessage2 = True
         else:
             raise Exception("Erroneous data received.")
-        data = data[1:]
-        data = bytes(data, 'ascii')
-        conn.sendall(data)
-    conn.close()
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+if __name__ == "__main__":
+    HOST = ''
+    PORT = 50007
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+
+    server_thread = threading.Thread(target=server.serve_forever)
+    server_thread.daemon = True
+    server_thread.start()
+
+    while True:
+        pass
